9c155a9b9928166c4d1978be130b27211b498b0b
Sam McNab <sam@sammcnab.co.uk>
2025-07-31 19:09:21 +0100

    feat: Add ratings

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -2,11 +2,23 @@ import { DocsLayout } from 'fumadocs-ui/layouts/docs';
 import type { ReactNode } from 'react';
 import { baseOptions } from '@/app/layout.config';
 import { source } from '@/lib/source';
+import { Rate } from '../components/rate';
+import posthog from 'posthog-js';
+import { PostHogProvider } from '../providers'
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
+    <PostHogProvider>
     <DocsLayout tree={source.pageTree} {...baseOptions}>
       {children}
+      <Rate
+        onRateAction={async (url, feedback) => {
+          'use server';
+          await posthog.capture('on_rate_docs', feedback);
+          return { githubUrl: 'https://github.com/your-repo/issues' };
+        }}
+      />
     </DocsLayout>
+    </PostHogProvider>
   );
 }
